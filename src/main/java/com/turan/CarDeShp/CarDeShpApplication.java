ace272326b3a00cdccd139ba0b26afdfefbbfaf2
kerimovturan <kerimovturan06@gmail.com>
2025-05-10 21:45:40 +0400

    Ilk commit

---
+++ src/main/java/com/turan/CarDeShp/CarDeShpApplication.java
@@ -0,0 +1,13 @@
+package com.turan.CarDeShp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDeShpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDeShpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turan/CarDeShp/CarDeShpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turan.CarDeShp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDeShpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
