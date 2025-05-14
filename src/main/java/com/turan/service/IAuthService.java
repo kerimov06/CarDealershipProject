00543b1dc664d0f64ac669b4e58284df1e5af41c
kerimovturan <kerimovturan06@gmail.com>
2025-05-15 02:58:59 +0400

    Authenticate yazildi ve token vaxti bitdikde ve s xetalari 401 kodu ile geri donmeyi ucun EntryPoint classi yazildi

--- src/main/java/com/turan/service/IAuthService.java
+++ src/main/java/com/turan/service/IAuthService.java
@@ -2,8 +2,10 @@ package com.turan.service;
 
 import com.turan.dto.DtoUser;
 import com.turan.jwt.AutRequest;
+import com.turan.jwt.AuthResponse;
 
 public interface IAuthService {
 
    public DtoUser register(AutRequest request);
+   public AuthResponse authenticate(AutRequest request);
 }
--- src/main/java/com/turan/service/impl/AuthService.java
+++ src/main/java/com/turan/service/impl/AuthService.java
@@ -2,14 +2,20 @@ package com.turan.service.impl;
 
 import com.turan.dto.DtoUser;
 import com.turan.jwt.AutRequest;
+import com.turan.jwt.AuthResponse;
+import com.turan.jwt.JwtService;
 import com.turan.model.User;
 import com.turan.repository.UserRepository;
 import com.turan.service.IAuthService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthService implements IAuthService {
@@ -20,17 +26,50 @@ public class AuthService implements IAuthService {
       @Autowired
       private BCryptPasswordEncoder passwordEncoder;
 
+      @Autowired
+      private AuthenticationProvider authenticationProvider;
+
+      @Autowired
+       private JwtService jwtService;
+
 
 
     @Override
     public DtoUser register(AutRequest request) {
            DtoUser dtoUser = new DtoUser();
-        User user = new User();
-        user.setUsername(request.getUsername());
-        user.setPassword(passwordEncoder.encode(request.getPassword()));
+           User user = new User();
+            user.setUsername(request.getUsername());
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
 
-         User dbUser = userRepository.save(user);
-        BeanUtils.copyProperties(dbUser,dtoUser);
-        return dtoUser;
+                 User saveUser =   userRepository.save(user);
+                 BeanUtils.copyProperties(saveUser,dtoUser);
+                 return dtoUser;
+    }
+
+    @Override
+    public AuthResponse authenticate(AutRequest request) {
+
+         try {
+             UsernamePasswordAuthenticationToken authenticationToken =
+                     new UsernamePasswordAuthenticationToken(request.getUsername(),request.getPassword());
+             authenticationProvider.authenticate(authenticationToken);
+
+                    Optional<User> optional = userRepository.findByUsername(request.getUsername());
+                         User user = optional.get();
+             boolean isPasswordMatch = passwordEncoder.matches(request.getPassword(), user.getPassword());
+             if (!isPasswordMatch) {
+                 throw new RuntimeException("Username or Password is incorrect");
+             }
+
+                    String token =   jwtService.generateToken(user);
+
+              return new AuthResponse(token);
+
+         }catch (Exception e){
+             throw new RuntimeException("Username or Password is wrong: " + e.getMessage());
+
+         }
+
+
     }
 }
